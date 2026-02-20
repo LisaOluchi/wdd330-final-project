2fb6097f996da8e92e08d0511f2700f7f2f85566
LisaOluchi <sangqulisa@gmail.com>
2026-02-20 22:17:11 +0200

    Final project complete - all features working

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/wdd330-final-project/',
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
   },
-})
+}) 
